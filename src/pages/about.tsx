14a577fa1c64bf579746a49e0c6fc09da3c69e99
Abdullatif-Erfan <abdullatif.erfan@gmail.com>
2023-06-08 09:23:32 +0430

    about page code is converted to typescript support code

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,7 @@
- function about(){
-    return <h1>about page</h1>
+import { NextPage } from "next";
+const about: NextPage<{}> = () => {
+    return <div>
+        <h1>about page</h1>
+    </div>
 }
 export default about
